b67240cf6c2e065a82340b143018f67d793a4733
Nathan Wisnoski <wisnoski@indiana.edu>
2022-04-20 20:45:48 -0600

    remade plots of variability partitions.

--- analysis/analyze_variability.R
+++ analysis/analyze_variability.R
@@ -15,7 +15,7 @@ variability %>%
   ggplot(aes(x = disp_rate, y = CV, color = as.factor(kernel_exp))) + 
   geom_point(alpha = 0.2) + 
   geom_smooth(se = FALSE) + 
-  facet_grid(variability~disturb_rate, scales = "free_y") +
+  facet_wrap(variability~disturb_rate, scales = "free_y") +
   scale_x_log10()
 
 variability %>% 
@@ -29,5 +29,35 @@ variability %>%
   ggplot(aes(x = disp_rate, y = phi, color = as.factor(kernel_exp))) + 
   geom_point(alpha = 0.2) + 
   geom_smooth(se = FALSE) + 
-  facet_grid(synchrony~disturb_rate, scales = "free_y") +
+  facet_wrap(synchrony~disturb_rate, scales = "free_y") +
+  scale_x_log10()
+
+
+
+variability %>% 
+  na.omit() %>% 
+  filter(disturb_rate < 0.1) %>% 
+  filter(condition == "priority") %>% 
+  pivot_longer(cols = c(CV_S_L, CV_C_L, CV_S_R, CV_C_R), 
+               names_to = "variability", values_to = "CV") %>% 
+  pivot_longer(cols = c(phi_S_L2R, phi_C_L2R), 
+               names_to = "synchrony", values_to = "phi") %>% 
+  ggplot(aes(x = disp_rate, y = CV, color = as.factor(kernel_exp))) + 
+  geom_point(alpha = 0.2) + 
+  geom_smooth(se = FALSE) + 
+  facet_wrap(variability~disturb_rate, scales = "free_y") +
+  scale_x_log10()
+
+variability %>% 
+  na.omit() %>% 
+  filter(disturb_rate < 0.1) %>% 
+  filter(condition == "priority") %>% 
+  pivot_longer(cols = c(CV_S_L, CV_C_L, CV_S_R, CV_C_R), 
+               names_to = "variability", values_to = "CV") %>% 
+  pivot_longer(cols = c(phi_S_L2R, phi_C_L2R), 
+               names_to = "synchrony", values_to = "phi") %>% 
+  ggplot(aes(x = disp_rate, y = phi, color = as.factor(kernel_exp))) + 
+  geom_point(alpha = 0.2) + 
+  geom_smooth(se = FALSE) + 
+  facet_wrap(synchrony~disturb_rate, scales = "free_y") +
   scale_x_log10()
