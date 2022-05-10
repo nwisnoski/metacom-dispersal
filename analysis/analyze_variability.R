8e97f96b5348fbb562a35ad033221584e1021678
Nathan Wisnoski <nathan.wisnoski@uwyo.edu>
2022-05-10 16:59:08 -0600

    investigating the species-to-community relationships and diversity-stability relationships in metacommunities

--- analysis/analyze_variability.R
+++ analysis/analyze_variability.R
@@ -4,28 +4,33 @@ library(here)
 variability <- read_csv(here("data/variability_data.csv"))
 
 
-variability %>% 
+var_long_stable <- variability %>% 
   na.omit() %>% 
   filter(disturb_rate < 0.1) %>% 
   filter(condition == "stable") %>% 
   pivot_longer(cols = c(CV_S_L, CV_C_L, CV_S_R, CV_C_R), 
                names_to = "variability", values_to = "CV") %>% 
-  pivot_longer(cols = c(phi_S_L2R, phi_C_L2R), 
-               names_to = "synchrony", values_to = "phi") %>% 
+  pivot_longer(cols = c(phi_S_L2R, phi_C_L2R, phi_S2C_L, phi_S2C_R), 
+               names_to = "synchrony", values_to = "phi") 
+
+var_long_priority <- variability %>% 
+  na.omit() %>% 
+  filter(disturb_rate < 0.1) %>% 
+  filter(condition == "priority") %>% 
+  pivot_longer(cols = c(CV_S_L, CV_C_L, CV_S_R, CV_C_R), 
+               names_to = "variability", values_to = "CV") %>% 
+  pivot_longer(cols = c(phi_S_L2R, phi_C_L2R, phi_S2C_L, phi_S2C_R), 
+               names_to = "synchrony", values_to = "phi") 
+
+
+var_long_stable %>% 
   ggplot(aes(x = disp_rate, y = CV, color = as.factor(kernel_exp))) + 
   geom_point(alpha = 0.2) + 
   geom_smooth(se = FALSE) + 
   facet_wrap(variability~disturb_rate, scales = "free_y") +
   scale_x_log10()
 
-variability %>% 
-  na.omit() %>% 
-  filter(disturb_rate < 0.1) %>% 
-  filter(condition == "stable") %>% 
-  pivot_longer(cols = c(CV_S_L, CV_C_L, CV_S_R, CV_C_R), 
-               names_to = "variability", values_to = "CV") %>% 
-  pivot_longer(cols = c(phi_S_L2R, phi_C_L2R), 
-               names_to = "synchrony", values_to = "phi") %>% 
+var_long_stable %>% 
   ggplot(aes(x = disp_rate, y = phi, color = as.factor(kernel_exp))) + 
   geom_point(alpha = 0.2) + 
   geom_smooth(se = FALSE) + 
@@ -34,30 +39,66 @@ variability %>%
 
 
 
-variability %>% 
-  na.omit() %>% 
-  filter(disturb_rate < 0.1) %>% 
-  filter(condition == "priority") %>% 
-  pivot_longer(cols = c(CV_S_L, CV_C_L, CV_S_R, CV_C_R), 
-               names_to = "variability", values_to = "CV") %>% 
-  pivot_longer(cols = c(phi_S_L2R, phi_C_L2R), 
-               names_to = "synchrony", values_to = "phi") %>% 
+var_long_priority %>% 
   ggplot(aes(x = disp_rate, y = CV, color = as.factor(kernel_exp))) + 
   geom_point(alpha = 0.2) + 
   geom_smooth(se = FALSE) + 
   facet_wrap(variability~disturb_rate, scales = "free_y") +
   scale_x_log10()
 
-variability %>% 
-  na.omit() %>% 
-  filter(disturb_rate < 0.1) %>% 
-  filter(condition == "priority") %>% 
-  pivot_longer(cols = c(CV_S_L, CV_C_L, CV_S_R, CV_C_R), 
-               names_to = "variability", values_to = "CV") %>% 
-  pivot_longer(cols = c(phi_S_L2R, phi_C_L2R), 
-               names_to = "synchrony", values_to = "phi") %>% 
+var_long_priority %>% 
   ggplot(aes(x = disp_rate, y = phi, color = as.factor(kernel_exp))) + 
   geom_point(alpha = 0.2) + 
   geom_smooth(se = FALSE) + 
   facet_wrap(synchrony~disturb_rate, scales = "free_y") +
   scale_x_log10()
+
+
+
+
+
+
+variability %>% 
+  filter(condition == "stable") %>% 
+  ggplot(aes(x = alpha_div, y = CV_S_L, color = as.factor(disp_rate))) + 
+  geom_point(alpha = 0.3) + 
+  #geom_smooth(se=FALSE) +
+  facet_grid(as.factor(kernel_exp) ~ disturb_rate, scales = "free")
+
+variability %>% 
+  filter(condition == "stable") %>% 
+  ggplot(aes(x = alpha_div, y = CV_C_L, color = as.factor(disp_rate))) + 
+  geom_point(alpha = 0.3) + 
+  #geom_smooth(se=FALSE) +
+  facet_grid(as.factor(kernel_exp) ~ disturb_rate, scales = "free")
+
+variability %>% 
+  filter(condition == "stable") %>% 
+  ggplot(aes(x = gamma_div, y = CV_S_R, color = as.factor(disp_rate))) + 
+  geom_point(alpha = 0.3) + 
+  #geom_smooth(se=FALSE) +
+  facet_grid(as.factor(kernel_exp) ~ disturb_rate, scales = "free")
+
+variability %>% 
+  filter(condition == "stable") %>% 
+  ggplot(aes(x = gamma_div, y = CV_C_R, color = as.factor(disp_rate))) + 
+  geom_point(alpha = 0.3) + 
+  #geom_smooth(se=FALSE) +
+  facet_grid(as.factor(kernel_exp) ~ disturb_rate, scales = "free")
+
+variability %>% 
+  filter(condition == "stable") %>% 
+  ggplot(aes(x = beta_div, y = phi_C_L2R, color = as.factor(disp_rate))) + 
+  geom_point(alpha = 0.3) + 
+  #geom_smooth(se=FALSE) +
+  facet_grid(as.factor(kernel_exp) ~ disturb_rate, scales = "free")
+
+variability %>% 
+  filter(condition == "stable") %>% 
+  filter(disturb_rate == 0) %>% 
+  ggplot(aes(x = alpha_div, y = CV_C_L, color = as.factor(kernel_exp))) + 
+  geom_point(alpha = 0.3) + 
+  geom_smooth(method = "lm", se=FALSE) +
+  facet_wrap(~as.factor(disp_rate), scales = "free") + 
+  scale_color_viridis_d() +
+  theme_minimal()
