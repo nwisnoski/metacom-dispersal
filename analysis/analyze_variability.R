62473b3086054c7a8556f1930a392f2d057e9f7d
Nathan Wisnoski <nathan.wisnoski@uwyo.edu>
2022-06-23 17:19:16 -0600

    visualizing the variability-dispersal relationships

--- analysis/analyze_variability.R
+++ analysis/analyze_variability.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(here)
 
+
+theme_set(theme_bw())
+
 variability <- read_csv(here("data/variability_data.csv"))
 
 
@@ -11,7 +14,8 @@ var_long_stable <- variability %>%
   pivot_longer(cols = c(CV_S_L, CV_C_L, CV_S_R, CV_C_R), 
                names_to = "variability", values_to = "CV") %>% 
   pivot_longer(cols = c(phi_S_L2R, phi_C_L2R, phi_S2C_L, phi_S2C_R), 
-               names_to = "synchrony", values_to = "phi") 
+               names_to = "synchrony", values_to = "phi") %>% 
+  mutate(variability = factor(variability, levels = c("CV_S_L", "CV_C_L", "CV_S_R", "CV_C_R")))
 
 var_long_priority <- variability %>% 
   na.omit() %>% 
@@ -23,35 +27,57 @@ var_long_priority <- variability %>%
                names_to = "synchrony", values_to = "phi") 
 
 
+# Plot figures
 var_long_stable %>% 
-  ggplot(aes(x = disp_rate, y = CV, color = as.factor(kernel_exp))) + 
+  #filter(disturb_rate == 0) %>% 
+  ggplot(aes(x = disp_rate, y = CV, color = as.factor(round(kernel_exp, 2)))) + 
   geom_point(alpha = 0.2) + 
   geom_smooth(se = FALSE) + 
-  facet_wrap(variability~disturb_rate, scales = "free_y") +
-  scale_x_log10()
+  facet_grid(round(disturb_rate,2)~variability, scales = "free_y") +
+  scale_x_log10() +
+  scale_color_viridis_d() + 
+  labs(color = "Dispersal kernel \nexponent",
+       x = "Emigration rate")
 
 var_long_stable %>% 
-  ggplot(aes(x = disp_rate, y = phi, color = as.factor(kernel_exp))) + 
+  filter(disturb_rate > 0) %>% 
+  ggplot(aes(x = disp_rate, y = CV, color = as.factor(round(kernel_exp, 2)))) + 
   geom_point(alpha = 0.2) + 
   geom_smooth(se = FALSE) + 
-  facet_wrap(synchrony~disturb_rate, scales = "free_y") +
-  scale_x_log10()
+  facet_grid(~variability, scales = "free_y") +
+  scale_x_log10() +
+  scale_color_viridis_d(option = "A") +
+  labs(color = "Dispersal kernel \nexponent",
+       x = "Emigration rate")
+
+var_long_stable %>% 
+  ggplot(aes(x = disp_rate, y = phi, color = as.factor(round(kernel_exp, 2)))) + 
+  geom_point(alpha = 0.2) + 
+  geom_smooth(se = FALSE) + 
+  facet_grid(synchrony~round(disturb_rate,2), scales = "free_y") +
+  scale_x_log10() +
+  labs(color = "Dispersal kernel \nexponent",
+       x = "Emigration rate")
 
 
 
 var_long_priority %>% 
-  ggplot(aes(x = disp_rate, y = CV, color = as.factor(kernel_exp))) + 
+  ggplot(aes(x = disp_rate, y = CV, color = as.factor(round(kernel_exp, 2)))) + 
   geom_point(alpha = 0.2) + 
   geom_smooth(se = FALSE) + 
-  facet_wrap(variability~disturb_rate, scales = "free_y") +
-  scale_x_log10()
+  facet_grid(variability~round(disturb_rate,2), scales = "free_y") +
+  scale_x_log10() +
+  labs(color = "Dispersal kernel \nexponent",
+       x = "Emigration rate")
 
 var_long_priority %>% 
-  ggplot(aes(x = disp_rate, y = phi, color = as.factor(kernel_exp))) + 
+  ggplot(aes(x = disp_rate, y = phi, color = as.factor(round(kernel_exp, 2)))) + 
   geom_point(alpha = 0.2) + 
   geom_smooth(se = FALSE) + 
-  facet_wrap(synchrony~disturb_rate, scales = "free_y") +
-  scale_x_log10()
+  facet_grid(synchrony~round(disturb_rate,2), scales = "free_y") +
+  scale_x_log10() +
+  labs(color = "Dispersal kernel \nexponent",
+       x = "Emigration rate")
 
 
 
@@ -102,3 +128,40 @@ variability %>%
   facet_wrap(~as.factor(disp_rate), scales = "free") + 
   scale_color_viridis_d() +
   theme_minimal()
+
+variability %>% 
+  filter(condition == "stable") %>% 
+  filter(disturb_rate == 0) %>% 
+  ggplot(aes(x = beta_div, y = phi_C_L2R, color = as.factor(kernel_exp))) + 
+  geom_point(alpha = 0.3) + 
+  geom_smooth(method = "lm", se=FALSE) +
+  facet_wrap(~as.factor(disp_rate), scales = "free") + 
+  scale_color_viridis_d() +
+  theme_minimal()
+
+variability %>% 
+  filter(condition == "stable") %>% 
+  filter(disturb_rate == 0) %>% 
+  ggplot(aes(x = gamma_div, y = CV_C_R, color = as.factor(kernel_exp))) + 
+  geom_point(alpha = 0.3) + 
+  geom_smooth(method = "lm", se=FALSE) +
+  facet_wrap(~as.factor(disp_rate), scales = "free") + 
+  scale_color_viridis_d() +
+  theme_minimal()
+
+
+
+variability %>% 
+  filter(condition == "stable") %>% 
+  filter(disturb_rate == 0) %>% 
+  ggplot(aes(x = alpha_div, y = CV_C_L, color = as.factor(disp_rate))) + 
+  geom_point(alpha = 0.3) + 
+  geom_smooth(method = "lm", se=FALSE) +
+  facet_wrap(~as.factor(round(kernel_exp,2)), scales = "free") + 
+  scale_color_viridis_d() +
+  theme_minimal() +
+  labs(color = "Emigration rate", 
+       x = "Mean alpha-diversity", 
+       y = "Local community variability")
+
+       
