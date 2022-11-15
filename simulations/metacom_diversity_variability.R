04434e5638ee5f657b6f94576ac9c203a9be03eb
Nathan Wisnoski <nathan.wisnoski@msstate.edu>
2022-11-15 11:56:37 -0600

    reduced disturbance rates

--- simulations/metacom_diversity_variability.R
+++ simulations/metacom_diversity_variability.R
@@ -43,7 +43,7 @@ set.seed(82072)
 
 disp_rates <- 10^seq(-5, 0, length.out = 20)
 kernel_vals <- seq(0, 1, length.out = 10)
-disturbance_rates <- seq(0, 0.5, length.out = 10)
+disturbance_rates <- seq(0.01, 0.5, length.out = 3)
 
 # remove seed bank
 germ <- 1
