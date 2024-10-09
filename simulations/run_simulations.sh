5de074da6c52f9f10cf3a0cf3db1882b411e3f56
Nathan Wisnoski <nathan.wisnoski@msstate.edu>
2024-10-09 17:28:43 -0500

    increase ram

--- simulations/run_simulations.sh
+++ simulations/run_simulations.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=64GB
+#SBATCH --mem=128GB
 #SBATCH --job-name=metacom_dispersal_dormancy
 
 cd /mnt/scratch/wisnoskilab/GitHub/mc-seedbank-dispersal
