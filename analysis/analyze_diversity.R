a768a41dc0e912fb6195cff1ccecf4ee075242f4
Nathan Wisnoski <nathan.wisnoski@msstate.edu>
2026-04-29 11:52:16 -0500

    final figs

--- analysis/analyze_diversity.R
+++ analysis/analyze_diversity.R
@@ -6,7 +6,7 @@ library(data.table)
 # Make figures for the simulation output from simulations/metacom_dispersal-kernel.R"
 theme_set(theme_bw())
 
-subfolder <- "sim_output/2026-04-28/"
+subfolder <- "sim_output/2026-04-29/"
 comp_scenario <- "stable"
 
 # load and combine reps
