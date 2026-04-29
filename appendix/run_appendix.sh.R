291f8ca199530755394b1905321748b3d8cca375
Nathan Wisnoski <nathan.wisnoski@msstate.edu>
2026-04-29 11:30:50 -0500

    refresh appendix

---
+++ appendix/run_appendix.sh.R
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=128GB
+#SBATCH --job-name=metacom_dispersal_dormancy
+
+cd /mnt/scratch/wisnoskilab/GitHub/metacom-dispersal
+
+module load R/4.4.0
+
+R CMD BATCH --no-restore --no-save appendix/metacom_dispersal-kernel_20sp.R
+R CMD BATCH --no-restore --no-save appendix/metacom_dispersal-kernel_75sp.R
